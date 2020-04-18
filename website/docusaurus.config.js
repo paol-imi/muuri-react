d4f17f2eaf14195931349494f9e39f550bb1e2a0
Paol-imi <paololongo.git@gmail.com>
2020-04-18 10:38:10 +0200

    Update documentation 📖

---
+++ website/docusaurus.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  title: "Muuri-react",
+  tagline: "The layout engine for React",
+  url: "https://paol-imi.github.io",
+  baseUrl: "/muuri-react/",
+  favicon: "logo/muuri-transparent.png",
+  organizationName: "paol-imi",
+  projectName: "muuri-react",
+  themeConfig: {
+    announcementBar: {
+      id: "supportus",
+      content:
+        '⭐️ If you like Muuri-react, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/paol-imi/muuri-react">GitHub</a>! ⭐️'
+    },
+    navbar: {
+      title: "Muuri-react",
+      logo: {
+        alt: "Muuri-react Logo",
+        src: "logo/muuri-transparent.png"
+      },
+      links: [
+        {
+          to: "docs/main-concepts/components",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left"
+        },
+        { to: "showcase", label: "Showcase", position: "left" },
+        {
+          href: "https://github.com/paol-imi/muuri-react",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Paol-imi. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/paol-imi/muuri-react/edit/master/website"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
---
+++ website/src/pages/showcase/index0.9.0.js
@@ -0,0 +1,161 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from "react";
+import Layout from "@theme/Layout"; // eslint-disable-line
+import classnames from "classnames";
+import styles from "./styles.module.css";
+
+const TITLE = "Showcase";
+const DESCRIPTION = "See the demos built with Muuri-react";
+
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+function getDescription(type) {
+  switch (type) {
+    case "Demo":
+      return "Demo showing Muuri-react features.";
+    case "Example":
+      return "Example showing how to implement a single Muuri-react functionality";
+    case "Implementation":
+      return "Implementation of a functionality with an external library.";
+    default:
+      return "";
+  }
+}
+
+const users = [
+  // Please add in alphabetical order of title.
+  {
+    type: "Demo",
+    name: "Grid",
+    id: "1czo5"
+  },
+  {
+    type: "Demo",
+    name: "Kanban",
+    id: "zmypd"
+  },
+  {
+    type: "Demo",
+    name: "Dashboard",
+    id: "penbe"
+  },
+  {
+    type: "Demo",
+    name: "Pokedex",
+    id: "swtf9"
+  },
+  {
+    type: "Example",
+    name: "Drag",
+    id: "xlix7"
+  },
+  {
+    type: "Example",
+    name: "Scroll",
+    id: "91ck8"
+  },
+  {
+    type: "Example",
+    name: "Sort",
+    id: "959o8"
+  },
+  {
+    type: "Example",
+    name: "Refresh",
+    id: "mz4di"
+  },
+  {
+    type: "Implementation",
+    name: "Resize",
+    id: "ycldc"
+  },
+  {
+    type: "Implementation",
+    name: "Media-query",
+    id: "q6sy6"
+  }
+].map((demo) => ({
+  type: demo.type,
+  title: `${demo.type} - ${demo.name}`,
+  description: getDescription(demo.type),
+  preview: `https://screenshots.codesandbox.io/${demo.id.toLowerCase()}.png`,
+  website: `https://${demo.id}.csb.app/`,
+  source: `https://codesandbox.io/s/muuri-react-${demo.name.toLowerCase()}-${
+    demo.id
+  }`,
+  fbOpenSource: false,
+  pinned: false
+}));
+
+function Showcase() {
+  return (
+    <Layout title={TITLE} description={DESCRIPTION}>
+      <div className="container margin-vert--lg">
+        <div className="text--center margin-bottom--xl">
+          <h1 style={{ fontSize: "46px" }}>{TITLE}</h1>
+          <p style={{ fontSize: "25px" }}>{DESCRIPTION}</p>
+        </div>
+        <div className="row">
+          {users.map((user) => (
+            <div key={user.title} className="col col--4 margin-bottom--lg">
+              <div className={classnames("card", styles.showcaseCard)}>
+                <div className="card__image">
+                  <img src={user.preview} alt={user.title} />
+                </div>
+                <div className="card__body">
+                  <div className="avatar">
+                    <div className="avatar__intro margin-left--none">
+                      <h4 className="avatar__name">{user.title}</h4>
+                      <small className="avatar__subtitle">
+                        {user.description}
+                      </small>
+                    </div>
+                  </div>
+                </div>
+                {(user.website || user.source) && (
+                  <div className="card__footer">
+                    <div className="button-group button-group--block">
+                      {user.website && (
+                        <a
+                          className="button button--small button--secondary button--block"
+                          href={user.website}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          Website
+                        </a>
+                      )}
+                      {user.source && (
+                        <a
+                          className="button button--small button--secondary button--block"
+                          href={user.source}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          Source
+                        </a>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Showcase;
